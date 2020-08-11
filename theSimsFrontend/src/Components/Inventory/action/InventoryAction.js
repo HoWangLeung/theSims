272c7faf53297127261be35a9e9de072c7a0b50b
Your Name <hkz88i00123@gmail.com>
2020-08-11 22:24:31 +0800

    update email

--- theSimsFrontend/src/Components/Inventory/action/InventoryAction.js
+++ theSimsFrontend/src/Components/Inventory/action/InventoryAction.js
@@ -7,7 +7,7 @@ export const fetchInventory= () => {
     console.log('sdfsfsdfsfdsdfdsf');
     return (dispatch, getState) => {
         
-        console.log('sdfsfsdfsfdsdfdsf222222');
+        console.log('sdfsfsdfsfdsdfdsf2d22222');
 
     }
 }
